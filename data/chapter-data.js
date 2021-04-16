c3f84ce9d20440c50204824ed8d9864c637dc792
xielingfan <xielingfan@bytedance.com>
2021-04-16 09:19:20 +0800

    1.4

--- data/chapter-data.js
+++ data/chapter-data.js
@@ -1,39 +1,39 @@
 var local_database = [
   {
       id: 0,
-      title: "第一章 吉他基础",
-      content: "吉他，又译为结他或六弦琴。是一种弹拨乐器，通常有六条弦，形状与提琴相似。",
-      imgSrc: "/images/guitar.png",
+      title: "第一章 如何用右手拨弦",
+      content: "从零开始，学习如何用右手拨弦，以及正确的手型",
+      imgSrc: "/images/chapter/1.jpg",
   },
   {
     id: 1,
-    title: "第二章 吉他进阶",
-    content: "吉他，又译为结他或六弦琴。是一种弹拨乐器，通常有六条弦，形状与提琴相似。",
-    imgSrc: "/images/guitar.png",
+    title: "第二章 右手分解练习",
+    content: "仅使用你的右手，弹响第一支旋律。",
+    imgSrc: "/images/chapter/2.jpg",
   },
   {
     id: 2,
-    title: "第三章",
-    content: "吉他，又译为结他或六弦琴。是一种弹拨乐器，通常有六条弦，形状与提琴相似。",
-    imgSrc: "/images/guitar.png",
+    title: "第三章 轻松的左手按弦方法",
+    content: "按不住? 弹不响? 正确的左手姿势让你事半功倍。",
+    imgSrc: "/images/chapter/3.jpg",
   },
   {
     id: 3,
-    title: "第四章",
-    content: "吉他，又译为结他或六弦琴。是一种弹拨乐器，通常有六条弦，形状与提琴相似。",
-    imgSrc: "/images/guitar.png",
+    title: "第四章 初次演奏——“小星星”",
+    content: "通过一首耳熟能详的歌曲，感受初次演奏的魅力。",
+    imgSrc: "/images/chapter/4.jpg",
   },
   {
     id: 4,
-    title: "第五章",
-    content: "吉他，又译为结他或六弦琴。是一种弹拨乐器，通常有六条弦，形状与提琴相似。",
-    imgSrc: "/images/guitar.png",
+    title: "第五章 最常用的7个和弦",
+    content: "学习最常见的几个和弦，以及流畅的轮换。",
+    imgSrc: "/images/chapter/5.jpg",
   },
   {
     id: 5,
-    title: "第六章",
-    content: "吉他，又译为结他或六弦琴。是一种弹拨乐器，通常有六条弦，形状与提琴相似。",
-    imgSrc: "/images/guitar.png",
+    title: "第六章 初次弹唱流行歌曲",
+    content: "七里香，不仅是夏天那么简单。",
+    imgSrc: "/images/chapter/6.jpg",
   }
 ]
 
--- data/section-data.js
+++ data/section-data.js
@@ -4,15 +4,21 @@ var local_database = [
       id: 0,
       recognizeId:0,
       title: "第一节 基本右手手型",
-      content: "本小节xxxxxxxxx",
+      content: "请按照示例图摆出手型：\n\
+                ① 手指各关节曲度自然\n \
+                ② 指尖“站立”在琴弦上，手指并拢\n \
+                ③ 拇指于低音琴弦夹角，30～45之间",
       imgSrc: "/images/guitar.png",
     },
     {
       id: 1,
       recognizeId:1,
-      title: "第二节 xxx",
-      content: "2吉他，又译为结他或六弦琴。是一种弹拨乐器，通常有六条弦，形状与提琴相似。",
-      imgSrc: "/images/guitar.png",
+      title: "第一节 基本右手手型",
+      content: "请按照示例图摆出手型：\n\
+                ① 手指各关节曲度自然\n \
+                ② 指尖“站立”在琴弦上，手指并拢\n \
+                ③ 拇指于低音琴弦夹角，30～45之间",
+      imgSrc: "/images/guitar2.jpg",
     },
   ],
   [
